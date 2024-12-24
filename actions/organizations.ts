ea3c91f753dab22f286d0bb49d4f0d1f17b60e02
= <ahmedjawad1857@gmail.com>
2024-12-24 18:25:08 +0500

    completed(now just AI integration left)

--- actions/organizations.ts
+++ actions/organizations.ts
@@ -1,75 +1,3 @@
-// "use server";
-
-// import { db } from "@/lib/prisma";
-// import { auth, clerkClient } from "@clerk/nextjs/server";
-
-// export const getOrganizations = async (slug: string) => {
-//   const { userId } = auth();
-//   if (!userId) {
-//     throw new Error("Unauthorized");
-//   }
-//   const user = await db?.user.findUnique({
-//     where: {
-//       clerkUserId: userId,
-//     },
-//   });
-//   if (!user) {
-//     throw new Error("User not found");
-//   }
-//   const organization = await clerkClient().organizations.getOrganization({
-//     slug,
-//   });
-//   if (!organization) {
-//     return null;
-//   }
-
-//   const { data: membership } =
-//     await clerkClient().organizations.getOrganizationMembershipList({
-//       organizationId: organization.id,
-//     });
-//   const userMembership = membership.find(
-//     (member) => member.publicUserData?.userId === userId
-//   );
-//   if (!userMembership) {
-//     return null;
-//   }
-//   return organization;
-// };
-
-// export const getOrganizationUsers = async (orgId: string) => {
-//   const { userId } = auth();
-//   if (!userId) {
-//     throw new Error("Unauthorized");
-//   }
-
-//   const user = await db?.user.findUnique({
-//     where: {
-//       clerkUserId: userId,
-//     },
-//   });
-//   if (!user) {
-//     throw new Error("User not found");
-//   }
-
-//   const organizationMembership =
-//     await clerkClient().organizations.getOrganizationMembershipList({
-//       organizationId: orgId,
-//     });
-
-//   // Check if the user is the only member
-//   const userIds = organizationMembership.data
-//     .map((member) => member.publicUserData?.userId)
-//     .filter((id): id is string => !!id);
-
-
-//   // Fetch all users if there are multiple members
-//   const users = await db?.user.findMany({
-//     where: {
-//       clerkUserId: { in: userIds },
-//     },
-//   });
-//   return users;
-// };
 
 "use server";
 
@@ -91,14 +19,14 @@ export async function getOrganizations(slug: string) {
       return { error: "User not found" };
     }
 
-    const organization = await clerkClient.organizations.getOrganization({ slug });
+    const organization = await clerkClient().organizations.getOrganization({ slug });
 
     if (!organization) {
       return { error: "Organization not found" };
     }
 
     const { data: membership } =
-      await clerkClient.organizations.getOrganizationMembershipList({
+      await clerkClient().organizations.getOrganizationMembershipList({
         organizationId: organization.id,
       });
 
